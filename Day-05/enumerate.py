411839e43743b2310d0718bd1f11cf20057762b7
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-05-17 10:14:34 +0530

    Created enumerate.py
    # Concept list pe loop chalate waqt index bhi chahiye ho to .
    #  Problem :fruits=["apple,"banana","mango"]
    #Task:Har fruit ko uske index ke saath print karo:"0:apple"
    fruits = ["apple", "banana", "mango"]
    for i, fruit in enumerate(fruits):
        print(f"{i}: {fruit}")
    # Output:
    # 0: apple
    # 1: banana  
    # 2: mango

---
+++ Day-05/enumerate.py
@@ -0,0 +1,10 @@
+# Concept list pe loop chalate waqt index bhi chahiye ho to .
+#  Problem :fruits=["apple,"banana","mango"]
+#Task:Har fruit ko uske index ke saath print karo:"0:apple"
+fruits = ["apple", "banana", "mango"]
+for i, fruit in enumerate(fruits):
+    print(f"{i}: {fruit}")
+# Output:
+# 0: apple
+# 1: banana  
+# 2: mango
