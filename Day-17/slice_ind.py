f3646e25bb09829bac818e2960551a8a4c68c719
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-05-30 18:58:36 +0530

    Update slice_ind.py

--- Day-17/slice_ind.py
+++ Day-17/slice_ind.py
@@ -1,11 +1,11 @@
 # Find index of element
 nums = [10, 20, 30]
 
-print(nums.index(20))
+print(nums.index(20))   # output 1
 
 
 
 # Slice a list
 nums = [1, 2, 3, 4, 5]
 
-print(nums[1:4])
+print(nums[1:4])          #output [ 2  3  4 ]
